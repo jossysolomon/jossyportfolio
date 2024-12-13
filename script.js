febff8075a156b671e21be9559e1e49294a7c513
jossysolomon <136467768+jossysolomon@users.noreply.github.com>
2024-12-13 09:08:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed (".multiple-text",{
+    strings: ["Frontend Developer", "Backend Developer",
+    "UI/UX Designer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
